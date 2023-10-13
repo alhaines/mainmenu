7162fec6d40a9d5a9cb40b585b9a7c936c877b73
Alfred j Haines <alfredhaines@gmail.com>
2023-10-13 01:47:46 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# Configuration for MySQL connection
+mysql_config = {
+    'host': 'localhost',
+    'user': 'user',
+    'password': 'password',
+    'database': 'database_name'
+}
+
+# Path to external hard drive change to your folder name
+external_hard_drive_path = '/media/user/Vault'
